495f4032092cd50ec760ed91e8f68ae35ddf3180
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-04-24 23:52:44 -0300

    Create ngrams

--- inc/main.hpp
+++ inc/main.hpp
@@ -1,7 +1,9 @@
 #ifndef __WIKIGEN_MAIN_H__
 #define __WIKIGEN_MAIN_H__
 
-#define LARGE_FILE "../xml/wikidump.xml"
-#define TINY_FILE  "../xml/wikidump-tiny.xml"
+#define LARGE_FILE       "../xml/wikidump.xml"
+#define TINY_FILE        "../xml/wikidump-tiny.xml"
+#define ULTRA_TINY_FILE  "../xml/wikidump-ultra-tiny.xml"
+#define N 4
 
 #endif/*__WIKIGEN_MAIN_H__*/
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,65 @@
 #include "../inc/tokenizer.hpp"
 #include "../inc/main.hpp"
 
+
+// Based on: https://github.com/errollw/gengram/blob/master/gengram.py
+std::vector<std::vector<std::string>> make_ngrams(std::vector<std::string>& corpus, unsigned int n) {
+	
+	std::vector<std::vector<std::string>> ngrams;
+	
+	for(unsigned int i = 0; i < corpus.size() - n + 1; i++) {
+		std::vector<std::string> words;
+
+		for(unsigned int j = 0; j < n; j++) {
+			words.push_back(corpus[i + j]);
+		}
+
+		ngrams.push_back(words);
+	}
+
+	return ngrams; // [[w1,w2,wn],[w19,w48,wn],...]
+}
+
+std::vector<std::vector<std::string>> make_ngrams_2(std::vector<std::string>& corpus, unsigned int n) {
+	
+	std::vector<std::vector<std::string>> ngrams;
+	
+	for(unsigned int i = 0; i < corpus.size() - n + 1; i++) {
+		std::vector<std::string> words;
+
+		words.insert(words.begin(), words.begin()+i, words.begin()+i+n);
+
+		ngrams.push_back(words);
+	}
+
+	return ngrams; // if n == 3: [[w0,w1,w2], [w1,w2,w3], ...]
+}
+
+void print_ngrams(std::vector<std::vector<std::string>>& ngrams) {
+	for(std::vector<std::string> ngram: ngrams) {
+		int idx = -1;
+		std::cout << "[";
+		for(std::string word: ngram) {
+			idx++;
+			if(idx == (int)ngram.size()-1) {
+				std::cout << word;
+			}
+			else
+				std::cout << word << " | ";
+		}
+		std::cout << "]" << std::endl;
+	}
+}
+
 int main(int argc, char const *argv[]) {
 
-	std::string filename = TINY_FILE;	
+	std::string filename = ULTRA_TINY_FILE;	
 	std::vector<std::string> mini_corpus = read_xml(filename);
+
+	unsigned int ngram_n = N;
+	std::vector<std::vector<std::string>> ngrams = make_ngrams(mini_corpus, ngram_n);
+
+	print_ngrams(ngrams);
 
     return 0;
 }
